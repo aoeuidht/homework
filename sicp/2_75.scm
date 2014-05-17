5121f19c55ab072f2c95315683cf93b60f04bb9e
lisztli <sockpuppet.lea@gmail.com>
2014-05-18 02:44:43 +0800

    add 2.75

---
+++ sicp/2_75.scm
@@ -0,0 +1,23 @@
+#lang racket
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+           (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+; Exercise 2.75. Implement the constructor make-from-mag-ang in message-passing style. 
+; This procedure should be analogous to the make-from-real-imag procedure given above.
+
+(define (make-from-mag-ang mag ang)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* mag (cos ang)))
+          ((eq? op 'imag-part) (* mag (sin ang)))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) ang)
+          (else (error "UNKNOWN OP -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)
