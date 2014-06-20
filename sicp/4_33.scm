6a6988b68f8dbc16f1a5cb5c11fc2e9fd9e68107
lisztli <sockpuppet.lea@gmail.com>
2014-06-20 23:31:51 +0800

    add(sipc) : 4.33

---
+++ sicp/4_33.scm
@@ -0,0 +1,21 @@
+#lang r5rs
+#|
+Exercise 4.33.  Ben Bitdiddle tests the lazy list implementation given above by evaluating the expression
+
+(car '(a b c))
+
+To his surprise, this produces an error. After some thought, 
+he realizes that the ``lists'' obtained by reading in quoted expressions
+ are different from the lists manipulated by the new definitions of cons,
+ car, and cdr. Modify the evaluator's treatment of quoted expressions so
+ that quoted lists typed at the driver loop will produce true lazy lists. 
+|#
+
+(define (text-of-quotation exp)
+  (define (quota-wrapper exp)
+    (if (null? (cdr exp))
+        (car exp)
+        (list 'cons (car exp) (quota-wrapper (cdr exp)))))
+  (quota-wrapper (cdr exp)))
+
+(display (text-of-quotation '(quote a b c)))
