28f365ecc30bf07e442d7d69759a4c2e58bff4b3
lisztli <sockpuppet.lea@gmail.com>
2014-12-16 15:28:14 +0800

    sicp

--- sicp/5_38.scm
+++ sicp/5_38.scm
@@ -34,14 +34,12 @@
                                         ; 5.38 d
 (define (expand-big-open-code exp)
   (let ((operator (car exp))
-        (oper-1st (cadr exp)))
-    (let ((oper-rst (if (> (length exp) 3)
-                        (expand-big-open-code (cons operator (cddr exp)))
-                        (caddr exp))))
-      (list operator oper-1st oper-rst))))
-
-
-
+        (oper-1st (cadr exp))
+        (oper-2nd (caddr exp)))
+    (if (> (length exp) 3)
+        (list operator (list operator oper-1st oper-2nd)
+              (cdddr exp))
+                exp)))
 
 (define (compile-open-code exp target linkage)
   (if (> (length exp) 3)
