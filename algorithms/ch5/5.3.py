e96811e8c8d6205c516f19b00a2966538ba78d52
lisztli <sockpuppet.lea@gmail.com>
2015-02-09 23:56:25 +0800

    add boyer-moore search

--- algorithms/ch5/5.3.py
+++ algorithms/ch5/5.3.py
@@ -29,14 +29,43 @@ class KMP:
             return i - M
         return N
 
+class BoyerMoore:
+    def __init__(self, pat):
+        self.pat = pat
+        M = len(pat)
+        R = 256
+        self.right = [-1] * 256
 
+        for j in range(M):
+            self.right[ord(pat[j])] = j
+
+    def search(self, txt):
+        N = len(txt)
+        M = len(self.pat)
+        skip = 0
+        i = 0
+        while i <= N-M:
+            skip = 0
+            for j in range(M-1, -1, -1):
+                if pat[j] != txt[i+j]:
+                    skip = j - self.right[ord(txt[i+j])]
+                    skip = skip if skip > 0 else 1
+                    break
+            if skip == 0:
+                return i
+            i += skip
 
 if __name__ == '__main__':
     pat = sys.argv[1]
     txt = sys.argv[2]
 
     kmp = KMP(pat)
+    bm = BoyerMoore(pat)
     offset = kmp.search(txt)
-    print "search %s in %s" % (pat, txt)
+    print "kmp search %s in %s" % (pat, txt)
+    print txt
+    print "%s %s" % (' ' * (offset-1), txt[offset: offset+len(pat)])
+    offset = bm.search(txt)
+    print "boyer-moore search %s in %s" % (pat, txt)
     print txt
     print "%s %s" % (' ' * (offset-1), txt[offset: offset+len(pat)])
