e17e0dbd82a4bfbdf5c9601f5f85dbd0973cfdc8
lisztli <sockpuppet.lea@gmail.com>
2015-02-24 00:47:27 +0800

    complete 4 sum

--- leetcode/four_sum.py
+++ leetcode/four_sum.py
@@ -8,76 +8,67 @@ import sys
 """
 
 class Solution:
+    def chk_cand(self, cand, rf, cf):
+        cand.sort()
+        k = '%d_%d_%d_%d' % tuple(cand)
+        if not rf.has_key(k):
+            rf[k] = 1
+            _rf = {}
+            v = True
+            for p in cand:
+                ck = p
+                _rc = _rf.get(ck, 0)
+                _rc += 1
+                if _rc > cf[ck]:
+                    v = False
+                    break
+                _rf[p] = _rc
+            if v:
+                return cand
+        return None
+
     # @return a list of lists of length 4, [[val1,val2,val3,val4]]
     def fourSum(self, num, target):
         nl = len(num)
         if nl < 4:
             return []
-        cand = []
         cf = {}
         cf[num[-1]] = 1
+        sum_dict = {}
         for i in range(nl-1):
             k =  num[i]
             ic = cf.get(k, 0)
             ic += 1
             cf[k] = ic
             for j in range(i+1, nl):
-                #l = num[i]
-                #s = num[j]
-                #l, s = (l, s) if l > s else (s, l)
-                #k = '%d_%d' % (l, s)
-                #if not cf.has_key(k):
-                #    cand.append((l+s, l))
-                #    cf[k] = 1
-                cand.append((num[i]+num[j], num[i]))
+                cand = sum_dict.get(num[i]+num[j], [])
+                cand.append([num[i], num[j]])
+                sum_dict[num[i]+num[j]] = cand
         # use the 2sum here get the four sum
-        cands = sorted(cand, key=lambda i: i[0])
-
+        cands = sum_dict.keys()
+        cands.sort()
         # remember the resurt calculated
         # use the sum-of-2 as the key, the offset of cands as value
         rst = []
         rf = {}
-        i, j = 0, nl * (nl - 1) / 2 - 1
-        cps = None
-        cps_chk = False
+        i, j = 0, len(cands) - 1
         while True:
             l, r = cands[i], cands[j]
-            #print i, j, l[0], r[0]
-            if (l[0] + r[0]) == target:
-                print i, j, l[0], r[0], l[1], l[0] - l[1], r[1], r[0] - r[1]
-                rst_cand = [l[1], l[0] - l[1], r[1], r[0] - r[1]]
-                rst_cand.sort()
-                k = '%d_%d_%d_%d' % tuple(rst_cand)
-                cps = (i, j-1)
-                cps_chk = False
-                i += 1
-                #j -= 1
-                print k
-                if not rf.has_key(k):
-                    rf[k] = 1
-                    _rf = {}
-                    v = True
-                    for p in rst_cand:
-                        ck = p
-                        _rc = _rf.get(ck, 0)
-                        _rc += 1
-                        if _rc > cf[ck]:
-                            v = False
-                            break
-                        _rf[p] = _rc
-                    if v:
+            if (l + r) == target:
+                # return all possible resurts
+                for c in [_i+_j for _i in sum_dict[l] for _j in sum_dict[r]]:
+                    rst_cand = self.chk_cand(c, rf, cf)
+                    if rst_cand:
                         rst.append(rst_cand)
-            elif (l[0] + r[0]) > target:
+                i += 1
+                j -= 1
+            elif (l + r) > target:
                 j -= 1
             else:
                 i += 1
-            if cps and cps_chk:
-                i, j = cps
-                cps = None
-            cps_chk = True
-            if (i >= j):
+            if (i > j):
                 break
-
+        rst.sort()
         return rst
 
 if __name__ == '__main__':
