7c5a52d01c17a9ae105c7d5bb1fb21e6ca6637cc
lisztli <sockpuppet.lea@gmail.com>
2015-05-16 23:54:35 +0800

    add eopl 3.12 ~ 3.18

---
+++ eopl/ch3/3.14/interp.rkt
@@ -0,0 +1,145 @@
+#lang eopl
+
+;; interpreter for the LET language.  The \commentboxes are the
+;; latex code for inserting the rules into the code in the book.
+;; These are too complicated to put here, see the text, sorry.
+
+(require "lang.rkt")
+(require "data-structures.rkt")
+(require "environments.rkt")
+
+(provide value-of-program value-of)
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+
+;; value-of-program : Program -> ExpVal
+;; Page: 71
+(define value-of-program 
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+                 (value-of exp1 (init-env))))))
+
+(define (arith-value-of op operator-exp operand-exp env)
+  (let ((val1 (value-of operator-exp env))
+        (val2 (value-of operand-exp env)))
+    (let ((num1 (expval->num val1))
+          (num2 (expval->num val2)))
+      (num-val (op num1 num2)))))
+
+(define (arith-compare-value-of op operator-exp operand-exp env)
+  (let ((val1 (value-of operator-exp env))
+        (val2 (value-of operand-exp env)))
+    (let ((num1 (expval->num val1))
+          (num2 (expval->num val2)))
+      (bool-val (op num1 num2)))))
+
+;; value-of : Exp * Env -> ExpVal
+;; Page: 71
+(define (value-of-bool-exp exp env)
+  (cases bool-exp exp
+         (unary-bool-exp
+          (op exp1)
+          (let ((val1 (value-of exp1 env)))
+            (bool-val
+             (cond
+              [(equal? op "zero?") (= 0 (expval->num val1))]
+              [(equal? op "null?") (null? (expval->list val1))]
+              [else (eopl:error 'invalid-unary-bool-exp-op op)]))))
+         (binary-bool-exp
+          (op exp1 exp2)
+          (let ((val1 (value-of exp1 env))
+                (val2 (value-of exp2 env)))
+            (bool-val
+             ((cond
+                [(equal? op "equal?") =]
+                [(equal? op "greater?") >]
+                [(equal? op "less?") <]
+                [else (eopl:error 'invalid-banary-bool-exp-op op)])
+              (expval->num val1)
+              (expval->num val2)
+              ))))))
+
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+      
+      ;;\commentbox{ (value-of (const-exp \n{}) \r) = \n{}}
+      (const-exp (num) (num-val num))
+      
+      ;;\commentbox{ (value-of (var-exp \x{}) \r) = (apply-env \r \x{})}
+      (var-exp (var) (apply-env env var))
+      
+      ;;\commentbox{\diffspec}
+      (diff-exp (exp1 exp2)
+                (let ((val1 (value-of exp1 env))
+                      (val2 (value-of exp2 env)))
+                  (let ((num1 (expval->num val1))
+                        (num2 (expval->num val2)))
+                    (num-val
+                     (- num1 num2)))))
+      
+      ;;\commentbox{\ma{\theifspec}}
+      (if-exp (exp1 exp2 exp3)
+              (let ((val1 (value-of-bool-exp exp1 env)))
+                (if (expval->bool val1)
+                    (value-of exp2 env)
+                    (value-of exp3 env))))
+
+      (b-exp (exp1)
+             (value-of-bool-exp exp1 env))
+
+      ;;\commentbox{\ma{\theletspecsplit}}
+      (let-exp (var exp1 body)       
+               (let ((val1 (value-of exp1 env)))
+                 (value-of body
+                           (extend-env var val1 env))))
+      ;; Add the minus operator
+      (minus-exp (exp1)
+                 (let ((val1 (value-of exp1 env)))
+                   (num-val (- 0 (expval->num val1)))))
+
+      (add-exp (exp1 exp2)
+               (arith-value-of + exp1 exp2 env))
+      
+      (mul-exp (exp1 exp2)
+               (arith-value-of * exp1 exp2 env))
+      
+      (div-exp (exp1 exp2)
+               (arith-value-of / exp1 exp2 env))
+
+      ;; the list related
+      (empty-list-exp () (list-val '()))
+      
+      (cons-exp (exp1 exp2)
+                (list-val
+                 (cons (value-of exp1 env)
+                       (expval->list (value-of exp2 env)))))
+
+      (car-exp (exp1)
+               (car (expval->list (value-of exp1 env))))
+      (cdr-exp (exp1)
+               (list-val  (cdr (expval->list (value-of exp1 env)))))
+
+      ;; the list expression
+      (list-exp (exp1)
+                (if (null? exp1)
+                    (list-val '())
+                    (list-val
+                      (cons (value-of (car exp1) env)
+                            (expval->list
+                             (value-of (list-exp (cdr exp1))
+                                       env)))))
+                )
+      ;; cond expression
+      (cond-exp (exp1 exp2)
+                (if (null? exp1)
+                    (eopl:error 'cond-exp-fail "no-cond-satisfied")
+                    (if (expval->bool (value-of-bool-exp (car exp1) env))
+                        (value-of (car exp2) env)
+                        (value-of (cond-exp (cdr exp1) (cdr exp2)) env)
+                        )
+                    ))
+      )))
+
+
---
+++ eopl/ch3/3.17/lang.rkt
@@ -0,0 +1,109 @@
+#lang eopl
+
+;; grammar for the LET language  
+
+(provide (all-defined-out))
+
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?" "+" "*" "/")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    (unary-bool-op ((or "zero?" "null?")) string)
+    (binary-bool-op ((or "equal?" "greater?"
+                         "less?")) string)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+    
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+    
+    (bool-exp
+     (unary-bool-op "(" expression ")") unary-bool-exp)
+    (bool-exp
+     (binary-bool-op "(" expression "," expression ")") binary-bool-exp)
+    
+    (expression
+     ("if" bool-exp "then" expression "else" expression)
+     if-exp)
+    
+    (expression (identifier) var-exp)
+    (expression (bool-exp) b-exp)
+    
+    (expression
+     ("let" (arbno identifier "=" expression) "in" expression)
+     let-exp)   
+
+    (expression
+     ("let*" (arbno identifier "=" expression) "in" expression)
+     let-star-exp)   
+
+    ;; add the minos operator
+    (expression
+     ("minus" "(" expression ")")
+     minus-exp)
+
+    ;; add addition
+    (expression
+     ("+" "(" expression "," expression ")")
+     add-exp)
+
+    ;; add multiplication
+    (expression
+     ("*" "(" expression "," expression ")")
+     mul-exp)
+
+    ;; add quotient
+    (expression
+     ("/" "(" expression "," expression ")")
+     div-exp)
+
+    ;; list related expression here
+    (expression
+     ("cons" "(" expression "," expression ")")
+     cons-exp)
+    (expression
+     ("car" "(" expression ")")
+     car-exp)
+    (expression
+     ("cdr" "(" expression ")")
+     cdr-exp)
+    (expression
+     ("emptylist")
+     empty-list-exp)
+
+    ;; the 'list keyword
+    (expression
+     ("list" "(" (separated-list expression ",") ")")
+     list-exp)
+
+    (expression
+     ("cond"
+      (arbno bool-exp "==>" expression)
+      "end")
+     cond-exp)
+    ))
+
+;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+
