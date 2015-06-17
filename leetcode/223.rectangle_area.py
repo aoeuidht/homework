8ef53810a114340269f8b8fec5b5dec8a1e88f1e
lisztli <sockpuppet.lea@gmail.com>
2015-06-18 00:11:10 +0800

    finish eopl 4.29; init eopl 4.30

--- leetcode/223.rectangle_area.py
+++ leetcode/223.rectangle_area.py
@@ -15,39 +15,18 @@ class Solution:
     # @param {integer} H
     # @return {integer}
     def computeArea(self, A, B, C, D, E, F, G, H):
-        # check if they has any overlap
-        area0 = self.gen_points(A, B, C, D)
-        area1 = self.gen_points(E, F, G, H)
-
-        area0, area1 = (area0, area1) if A <= E else (area1, area0)
-
-        a0_chk = map(lambda p: self.point_in_area(p, area1),
-                     area0)
-        a1_chk = map(lambda p: self.point_in_area(p, area0),
-                     area1)
-
+        x_left = max(A, E)
+        x_right = min(C, G)
+        y_bottom = max(B, F)
+        y_top = min(D, H)
         area0 = (C - A) * (D - B)
         area1 = (G - E) * (H - F)
-        if (not any(a0_chk)) and (not any(a1_chk)):
-            # a + b
-            return area0 + area1
-        elif ((not any(a0_chk)) and all(a1_chk)):
-            return area0
-        elif ((not any(a1_chk)) and all(a0_chk)):
-            return area1
-        # here the most complex case
 
-
-    def point_in_area(point, area):
-        x, y = point
-        x0, y0 = area[0]
-        x1, y1 = area[2]
-        return (x0 <= x <= x1) and (y0 <= y <= y1)
-
-    def gen_points(self, A, B, C, D):
-        # return left-bottom, right-bottom, right-top, left-top
-        # points with format (x, y)
-        return ((A, B), (C, B), (C, D), (A, D))
+        overlap = 0
+        if (x_left < x_right) and (y_bottom < y_top):
+            overlap = (x_right - x_left) * (y_top - y_bottom)
+        return area0 + area1 - overlap
 
 if __name__ == '__main__':
     s = Solution()
+    print s.computeArea(-3, 0, 3, 4, 0, -1, 9, 2)
