cb7b164a56f953d49c343ecffa36b09e1e956775
lisztli <sockpuppet.lea@gmail.com>
2015-06-20 00:53:49 +0800

    add eopl 4.37

--- eopl/ch4/4.37/interp.rkt
+++ eopl/ch4/4.37/interp.rkt
@@ -14,7 +14,7 @@
 
 ;;;;;;;;;;;;;;;; switches for instrument-let ;;;;;;;;;;;;;;;;
 
-(define instrument-let (make-parameter #f))
+(define instrument-let (make-parameter #t))
 
 ;; say (instrument-let #t) to turn instrumentation on.
 ;;     (instrument-let #f) to turn it off again.
@@ -23,9 +23,9 @@
 ;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
 
 ;; value-of-program : Program -> ExpVal
-(define value-of-program 
+(define value-of-program
   (lambda (pgm)
-    (initialize-store!)             
+    (initialize-store!)
     (cases program pgm
       (a-program (body)
                  (value-of body (init-env))))))
@@ -35,11 +35,11 @@
 (define value-of
   (lambda (exp env)
     (cases expression exp
-      
+
       (const-exp (num) (num-val num))
-      
+
       (var-exp (var) (deref (apply-env env var)))
-      
+
       (diff-exp (exp1 exp2)
                 (let ((val1
                        (expval->num
@@ -49,27 +49,27 @@
                         (value-of exp2 env))))
                   (num-val
                    (- val1 val2))))
-      
+
       (zero?-exp (exp1)
                  (let ((val1 (expval->num (value-of exp1 env))))
                    (if (zero? val1)
                        (bool-val #t)
                        (bool-val #f))))
-      
-      (if-exp (exp0 exp1 exp2) 
+
+      (if-exp (exp0 exp1 exp2)
               (if (expval->bool (value-of exp0 env))
                   (value-of exp1 env)
                   (value-of exp2 env)))
-      
-      
+
+
       ;; straightforward version of LET, without instrumentation
-      ;; (let-exp (id rhs body)       
+      ;; (let-exp (id rhs body)
       ;;   (let ((val (value-of rhs env)))
       ;;     (value-of body
       ;;       (extend-env id (newref val) env))))
-      
+
       ;; LET with instrumentation
-      (let-exp (var exp1 body)       
+      (let-exp (var exp1 body)
                (when (instrument-let)
                  (eopl:printf "entering let ~s~%" var))
                (let ((val (value-of exp1 env)))
@@ -82,22 +82,23 @@
                      (eopl:printf "~%")
                      )
                    (value-of body new-env))))
-      
+
       (proc-exp (var body)
                 (proc-val
                  (procedure var body env)))
-      
+
       (call-exp (rator rand)
                 (let ((proc (expval->proc (value-of rator env)))
                       (arg (value-of-operand rand env)))
+                  (eopl:printf "the arg are: ~s~%" proc)
                   (apply-procedure proc arg)))
-      
+
       (letrec-exp (p-names b-vars p-bodies letrec-body)
                   (value-of letrec-body
                             (extend-env-rec* p-names b-vars p-bodies env)))
-      
+
       (begin-exp (exp1 exps)
-                 (letrec 
+                 (letrec
                      ((value-of-begins
                        (lambda (e1 es)
                          (let ((v1 (value-of e1 env)))
@@ -105,24 +106,24 @@
                                v1
                                (value-of-begins (car es) (cdr es)))))))
                    (value-of-begins exp1 exps)))
-      
+
       (assign-exp (x e)
                   (begin
                     (setref!
                      (apply-env env x)
                      (value-of e env))
                     (num-val 27)))
-      
+
       (newpair-exp (exp1 exp2)
                    (let ((v1 (value-of exp1 env))
                          (v2 (value-of exp2 env)))
                      (mutpair-val (make-pair v1 v2))))
-      
+
       (left-exp (exp1)
                 (let ((v1 (value-of exp1 env)))
                   (let ((p1 (expval->mutpair v1)))
                     (left p1))))
-      
+
       (setleft-exp (exp1 exp2)
                    (let ((v1 (value-of exp1 env))
                          (v2 (value-of exp2 env)))
@@ -130,12 +131,12 @@
                        (begin
                          (setleft p v2)
                          (num-val 82)))))
-      
+
       (right-exp (exp1)
                  (let ((v1 (value-of exp1 env)))
                    (let ((p1 (expval->mutpair v1)))
                      (right p1))))
-      
+
       (setright-exp (exp1 exp2)
                     (let ((v1 (value-of exp1 env))
                           (v2 (value-of exp2 env)))
@@ -143,7 +144,7 @@
                         (begin
                           (setright p v2)
                           (num-val 83)))))
-      
+
       )))
 
 ;; apply-procedure : Proc * Ref -> ExpVal
@@ -161,9 +162,13 @@
 ;; instrumented version
 (define apply-procedure
   (lambda (proc1 val)
+    (when (instrument-let)
+          (eopl:printf "proc ~s ~%" proc1)
+          (eopl:printf "val ~s ~%" val)
+          )
     (cases proc proc1
       (procedure (var body saved-env)
-                 (let ((new-env (extend-env var val saved-env)))
+                 (let ((new-env (extend-env var (car val) saved-env)))
                    (when (instrument-let)
                      (eopl:printf
                       "entering body of proc ~s with env =~%"
@@ -172,23 +177,30 @@
                      (eopl:printf "store =~%")
                      (pretty-print (store->readable (get-store-as-list)))
                      (eopl:printf "~%"))
-                   (value-of body new-env))))))
+                   (let ((rst (value-of body new-env))
+                         (param-addr (cdr val)))
+                     (when (pair? param-addr)
+                           (setref! (car param-addr) (deref (car val))))
+                     rst
+                     ))))))
 
 
 ;; value-of-rand : Exp * Env -> Ref
 ;; Page: 132
 ;; if the expression is a var-exp, then pass the reference.
 ;; otherwise, evaluate the expression and pass a reference to a new
-;; cell. 
+;; cell.
 
 (define value-of-operand
   (lambda (exp env)
-    (cases expression exp
-      (var-exp (var) (apply-env env var)) 
-      (else
-       (newref (value-of exp env))))))
+    (let ((val (newref (value-of exp env))))
+      (cases expression exp
+             (var-exp (var) (list val (apply-env env var)))
+             (else
+              (list val)
+              )))))
 
-;; store->readable : Listof(List(Ref,Expval)) 
+;; store->readable : Listof(List(Ref,Expval))
 ;;                    -> Listof(List(Ref,Something-Readable))
 (define store->readable
   (lambda (l)
@@ -198,9 +210,3 @@
         (car p)
         (expval->printable (cadr p))))
      l)))
-
-
-
-
-
-
