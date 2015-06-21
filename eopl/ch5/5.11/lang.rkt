5cd6a723a82d42f92b8103baa0f91058d1efe6b7
lisztli <sockpuppet.lea@gmail.com>
2015-06-21 23:24:38 +0800

    add eopl 5.11

---
+++ eopl/ch5/5.11/lang.rkt
@@ -0,0 +1,93 @@
+#lang eopl
+
+;; grammar for the LETREC language
+
+(provide (all-defined-out))
+
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    (expression
+     ("proc" "(" identifier ")" expression)
+     proc-exp)
+
+    (expression
+     ("(" expression expression ")")
+     call-exp)
+
+    (expression
+     ("letrec"
+      identifier "(" identifier ")" "=" expression
+      "in" expression)
+     letrec-exp)
+    ;; list related expression here
+    ;; (expression
+    ;;  ("cons" "(" expression "," expression ")")
+    ;;  cons-exp)
+    (expression
+     ("car" "(" expression ")")
+     car-exp)
+    (expression
+     ("cdr" "(" expression ")")
+     cdr-exp)
+    ;; (expression
+    ;;  ("null?" "(" expression ")")
+    ;;  null?-exp)
+    (expression
+     ("emptylist")
+     empty-list-exp)
+
+    ;; the 'list keyword
+    (expression
+     ("list" "(" (separated-list expression ",") ")")
+     list-exp)
+    ;; the begin keyword
+    (expression
+     ("begin" (separated-list expression ";") "end")
+     begin-exp)
+
+    ))
+
+;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
