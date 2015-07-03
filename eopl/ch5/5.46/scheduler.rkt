a6d88ce321d1c6b35504f3acffcd98692d7eec55
lisztli <sockpuppet.lea@gmail.com>
2015-07-03 15:31:47 +0800

    finish eopl 5.46

--- eopl/ch5/5.46/scheduler.rkt
+++ eopl/ch5/5.46/scheduler.rkt
@@ -6,21 +6,23 @@
 
 (provide
  initialize-scheduler!
- set-final-answer! 
- 
+ set-final-answer!
+
  time-expired?
  decrement-timer!
- 
+
  place-on-ready-queue!
  run-next-thread
- 
+
+ get-time-remaining
+ restore-time-remaining!
  )
 
 ;;;;;;;;;;;;;;;; the state ;;;;;;;;;;;;;;;;
 
 ;; components of the scheduler state:
 
-(define the-ready-queue   'uninitialized)         
+(define the-ready-queue   'uninitialized)
 (define the-final-answer  'uninitialized)
 
 (define the-max-time-slice    'uninitialized)
@@ -32,46 +34,54 @@
     (set! the-ready-queue (empty-queue))
     (set! the-final-answer 'uninitialized)
     (set! the-max-time-slice ticks)
-    (set! the-time-remaining the-max-time-slice) 
+    (set! the-time-remaining the-max-time-slice)
     ))
 
 ;;;;;;;;;;;;;;;; the final answer ;;;;;;;;;;;;;;;;
 
 ;; place-on-ready-queue! : Thread -> Unspecified
-;; Page: 184  
+;; Page: 184
 (define place-on-ready-queue!
   (lambda (th)
     (set! the-ready-queue
           (enqueue the-ready-queue th))))
 
 ;; run-next-thread : () -> FinalAnswer
-;; Page: 184    
+;; Page: 184
 (define run-next-thread
   (lambda ()
     (if (empty? the-ready-queue)
         the-final-answer
         (dequeue the-ready-queue
                  (lambda (first-ready-thread other-ready-threads)
-                   (set! the-ready-queue other-ready-threads)            
-                   (set! the-time-remaining the-max-time-slice) 
+                   (set! the-ready-queue other-ready-threads)
+                   (set! the-time-remaining the-max-time-slice)
                    (first-ready-thread)
                    )))))
 
 ;; set-final-answer! : ExpVal -> Unspecified
-;; Page: 184    
+;; Page: 184
 (define set-final-answer!
   (lambda (val)
     (set! the-final-answer val)))
 
 ;; time-expired? : () -> Bool
-;; Page: 184    
+;; Page: 184
 (define time-expired?
   (lambda ()
     (zero? the-time-remaining)))
 
 ;; decrement-timer! : () -> Unspecified
-;; Page: 184    
+;; Page: 184
 (define decrement-timer!
   (lambda ()
     (set! the-time-remaining (- the-time-remaining 1))))
 
+
+;;; add the yield releated operations
+
+(define (get-time-remaining)
+  the-time-remaining)
+
+(define (restore-time-remaining! tr)
+  (set! the-time-remaining tr))
