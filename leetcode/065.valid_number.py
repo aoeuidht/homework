04f7755796a937aad7c58f8709d353d8649f3286
lisztli <sockpuppet.lea@gmail.com>
2015-08-13 23:14:06 +0800

    [leetcode] complete puzzel 065 about the float number

--- leetcode/065.valid_number.py
+++ leetcode/065.valid_number.py
@@ -17,7 +17,83 @@ class Solution:
         except:
             return False
 
+    def isNumber1(self, s):
+        s = s.strip()
+        if not s:
+            return False
+
+        stat = 0
+        i = 0
+        j = len(s)
+        c = s[i]
+        if c in ('+', '-'):
+            i += 1
+            c = s[i]
+
+        # 1123
+        if '0' <= c <= '9':
+            i += 1
+            # loop until we met a non-digital number
+            while (i < j) and ('0' <= s[i] <= '9'):
+                i += 1
+            if i >= j:
+                return True
+        elif c == '.':
+            i += 1
+            if i >= j:
+                return False
+            if not ('0' <= s[i] <= '9'):
+                return False
+            i -= 1
+        else:
+            return False
+
+        # next should be . or eE
+        if i >= j:
+            return True
+
+        c = s[i]
+        if c == '.':
+            i += 1
+            while (i < j) and ('0' <= s[i] <= '9'):
+                i += 1
+            if i >= j:
+                return True
+
+        if s[i] not in ('e', 'E'):
+            return False
+        i += 1
+        if i >= j:
+            return False
+        if s[i] in ('+', '-'):
+            i += 1
+        if i >= j:
+            return False
+        while (i < j) and ('0' <= s[i] <= '9'):
+            i += 1
+        if i >= j:
+            return True
+        return False
+
 if __name__ == '__main__':
     s = Solution()
-    v = lambda st: (st, s.isNumber(st))
-    print v('hehehe')
+    scenarios = ['0.0',
+                 '.0',
+                 '00',
+                 '01',
+                 '1.2',
+                 '2.3e0',
+                 '2.0e',
+                 '2.0e0',
+                 '4e0',
+                 '4e-0',
+                 '-4e0',
+                 '.0e45',
+                 '3.',
+                 '1 a ',
+                 '01']
+
+
+    v = lambda st: (st, s.isNumber1(st), s.isNumber(st))
+    for r in map(v, scenarios):
+        print r
