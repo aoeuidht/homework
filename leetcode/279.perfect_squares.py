dd95efca1785e948b820e9ba20bb5e0710e57375
lisztli <sockpuppet.lea@gmail.com>
2015-09-10 00:32:26 +0800

    [leetcode] still got tle

--- leetcode/279.perfect_squares.py
+++ leetcode/279.perfect_squares.py
@@ -7,30 +7,67 @@ from oj_helper import *
 
 
 class Solution(object):
-    def numSquares(self, n):
+    def numSquares1(self, n):
         """
         :type n: int
         :rtype: int
         """
-        p = n ** 0.5
-        if n == (p ** 2):
+        p = int(n ** 0.5)
+        if n == (p * p):
             return 1
+
         cache = {0: 0,
                  1: 1}
-        for i in range(2, n+1):
-            cache[i] = i
-            for j in range(1, int(i ** 0.5) + 1):
-                lookup = i - j ** 2
-                if lookup:
-                    cache[i] = min(cache[i],
-                                   cache[i - j **2] + 1)
-                else:
-                    cache[i] = 1
+
+        for i in xrange(2, n+1):
+            p = int(i ** 0.5)
+            if i == p * p:
+                cache[i] = 1
+                continue
+            else:
+                cache[i] = i
+
+            for j in xrange(p, 0, -1):
+                lookup = i - j * j
+                tgt = cache[lookup] + 1
+                if cache[i] > tgt:
+                    cache[i] = tgt
+                if cache[i] == 2:
                     break
 
         return cache[n]
+
+    def numSquares(self, n):
+        """
+
+        Arguments:
+        - `self`:
+        - `n`:
+        """
+        cache = [n] * (n + 1)
+        cache[0] = 0
+        cache[1] = 1
+
+        p = int(n ** 0.5)
+        if p * p == n:
+            return 1
+
+        for i in range(0, n+1):
+            for j in range(0, p+1):
+                nxt = i + j * j
+                if nxt > n:
+                    break
+                if cache[nxt] > cache[i] + 1:
+                    cache[nxt] = cache[i] + 1
+        return cache[n]
+
 
 
 if __name__ == '__main__':
     s = Solution()
     print s.numSquares(8285)
+    print s.numSquares(8829)
+    """
+    for x in range(1000):
+        print x, s.numSquares(x)
+    """
