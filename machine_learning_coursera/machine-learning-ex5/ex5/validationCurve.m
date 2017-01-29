47c45eeb706ef1d455d715644b09874a159c0d4b
lisztli <sockpuppet.lea@gmail.com>
2017-01-29 14:18:51 +0800

    machine learning -- ex5

---
+++ machine_learning_coursera/machine-learning-ex5/ex5/validationCurve.m
@@ -0,0 +1,60 @@
+function [lambda_vec, error_train, error_val] = ...
+    validationCurve(X, y, Xval, yval)
+%VALIDATIONCURVE Generate the train and validation errors needed to
+%plot a validation curve that we can use to select lambda
+%   [lambda_vec, error_train, error_val] = ...
+%       VALIDATIONCURVE(X, y, Xval, yval) returns the train
+%       and validation errors (in error_train, error_val)
+%       for different values of lambda. You are given the training set (X,
+%       y) and validation set (Xval, yval).
+%
+
+% Selected values of lambda (you should not change this)
+lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+
+% You need to return these variables correctly.
+error_train = zeros(length(lambda_vec), 1);
+error_val = zeros(length(lambda_vec), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return training errors in 
+%               error_train and the validation errors in error_val. The 
+%               vector lambda_vec contains the different lambda parameters 
+%               to use for each calculation of the errors, i.e, 
+%               error_train(i), and error_val(i) should give 
+%               you the errors obtained after training with 
+%               lambda = lambda_vec(i)
+%
+% Note: You can loop over lambda_vec with the following:
+%
+%       for i = 1:length(lambda_vec)
+%           lambda = lambda_vec(i);
+%           % Compute train / val errors when training linear 
+%           % regression with regularization parameter lambda
+%           % You should store the result in error_train(i)
+%           % and error_val(i)
+%           ....
+%           
+%       end
+%
+%
+
+m = length(X);
+X = [ones(m, 1) X];
+m_val = length(Xval);
+Xval = [ones(m_val, 1) Xval];
+
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  [theta] = trainLinearReg(X, y, lambda);
+
+  h_theta = X * theta;
+  error_train(i) = sum((h_theta - y) .* (h_theta - y)) / 2 / m;
+  
+  h_theta_val = Xval * theta;
+  error_val(i) = sum((h_theta_val - yval) .* (h_theta_val - yval)) / 2 / m_val;
+end
+
+% =========================================================================
+
+end
